872a26199fcb751d0758c3e329e50452169c2765
Mathew Arnold <mattyarnold@hotmail.co.uk>
2026-04-20 18:04:40 +0100

    started code

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] private AudioSource _solarWindAS;
+    [SerializeField] private AudioSource _satellitePingAS;
     [SerializeField] private GameObject _planetAudioPrefab;
     private float _solarWindTargetVolume = 0;
     private float _solarWindMaxVolume = 0.2f;
@@ -98,6 +99,12 @@ public class AudioManager : MonoBehaviour
                 planetAudio.SetVolumes(0, 0);
             }
         }
+    }
+    
+    
+    public void PlaySatellitePing()
+    {
+        _satellitePingAS.Play();
     }
     
     
